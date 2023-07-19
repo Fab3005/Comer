52a3794e58a1e1f3141feae00b4b3711589ed477
Fab3005 <fabrizzio7112@outlook.com>
2023-07-19 19:54:11 -0400

    try

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,5 +10,17 @@ userController.createUser = async (req, res, next) => {
             message: { err: 'An error occurred' }
         });
     }
-
+    try {
+        console.log(username , password)
+        res.locals.user = req.body;
+        const user = await User.create({ username, password });
+        res.locals.true = true;
+        return next();
+    } catch (err) {
+        return next({
+            log: 'Error occurred in userController.createUser',
+            status: 400,
+            message: { err: 'An error occurred' },
+        });
+    }
 };
