7367477ab884b4b644a854b6c26787bd2a56b4cc
Fab3005 <fabrizzio7112@outlook.com>
2023-07-19 19:53:55 -0400

    f

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,5 +2,13 @@ const User = require('../models/userModel.js');
 const userController = {};
 
 userController.createUser = async (req, res, next) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return next({
+            log: 'Error occurred in userController.createUser',
+            status: 400,
+            message: { err: 'An error occurred' }
+        });
+    }
 
 };
