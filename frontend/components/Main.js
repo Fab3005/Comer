9c13b0b7ca916003a3631ac79e2e562cfd11f970
Fab3005 <fabrizzio7112@outlook.com>
2023-07-19 20:04:32 -0400

    main

---
+++ frontend/components/Main.js
@@ -0,0 +1,6 @@
+import React, {useState} from 'react';
+import './CSS/recipes.css';
+import mouth from './mouthWater.svg';
+import Axios from 'axios';
+import Recipe from './recipes';
+import {v4 as uuidv4} from 'uuid'
