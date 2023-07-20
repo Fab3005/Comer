69749b8c043c60cdf0476da2c24b8b651ee81f0b
Fab3005 <fabrizzio7112@outlook.com>
2023-07-19 20:07:03 -0400

    a

--- frontend/components/Main.js
+++ frontend/components/Main.js
@@ -4,3 +4,9 @@ import mouth from './mouthWater.svg';
 import Axios from 'axios';
 import Recipe from './recipes';
 import {v4 as uuidv4} from 'uuid'
+
+const Main = () => {
+ 
+}
+
+export default Main;
